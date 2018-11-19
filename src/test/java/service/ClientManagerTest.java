40b6c8ee19678d994b7715421f65b453c0f6b025
Dirk-Jan de Beijer <djjadebeijer@gmail.com>
2018-11-19 17:27:24 +0100

    Database testen erbij
    Maar uitgecommented omdat er nog een aparte database voor het testen moet komen

---
+++ src/test/java/service/ClientManagerTest.java
@@ -0,0 +1,26 @@
+package service;
+
+import authentication.Account;
+import domain.Client;
+import domain.Gender;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class ClientManagerTest {
+
+//    @Test
+//    public void addClient() {
+//        ClientManager mgr = new ClientManager();
+//
+//        Client client = new Client("Dirk-Jan", new Date(), Gender.MALE, "bla bla bla help hem");
+//
+//        Account account = new Account("dj", "dj");
+//
+//        client.setAccount(account);
+//
+//        mgr.addClient(client);
+//    }
+}
---
+++ src/test/java/service/HealthRatingManagerTest.java
@@ -0,0 +1,22 @@
+package service;
+
+import domain.HealthRating;
+import org.junit.Test;
+
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+public class HealthRatingManagerTest {
+
+//    @Test
+//    public void addHealthRating() {
+//        HealthRatingManager mgr = new HealthRatingManager();
+//
+//        HealthRating hr = new HealthRating();
+//        hr.setRating(2);
+//        hr.setDate(new Date());
+//
+//        mgr.addHealthRating(hr);
+//    }
+}
